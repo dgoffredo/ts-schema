fb0691246180c6153f3c8fcc4ef1be579d951d55
David Goffredo <david.goffredo@datadoghq.com>
2022-04-15 22:04:52 +0000

    fix the lexer

--- example/schema.ts
+++ example/schema.ts
@@ -1,6 +1,6 @@
 // A Person is a whatever.  They're like whatever, this is a comment, and it
 // doesn't break my script.  Isn't that grand?
-interface /* Here is another comment, like WFT? */ Person {
+interface /* Here is another comment, like WFT? */ Person{
 	firstName: string,
 	lastName: string,
 	age?: number
