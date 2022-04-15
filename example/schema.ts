8ff8a9f4ee8de144e308888e4efd8e1705c7be4d
David Goffredo <david.goffredo@datadoghq.com>
2022-04-15 22:03:08 +0000

    test the lexer

--- example/schema.ts
+++ example/schema.ts
@@ -1,4 +1,6 @@
-interface Person {
+// A Person is a whatever.  They're like whatever, this is a comment, and it
+// doesn't break my script.  Isn't that grand?
+interface /* Here is another comment, like WFT? */ Person {
 	firstName: string,
 	lastName: string,
 	age?: number
